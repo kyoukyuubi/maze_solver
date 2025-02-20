701e97eb313c4d41c00d82edac6652909747652e
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-02-21 00:08:38 +0100

    Added unittest for a method that resets the .visited value

--- maze.py
+++ maze.py
@@ -26,6 +26,7 @@ class Maze:
         self._create_cells()
         self._break_entrance_and_exit()
         self._break_walls_r(0, 0)
+        self._reset_cells_visited()
 
     def _create_cells(self):
         self._cells = []
@@ -112,3 +113,8 @@ class Maze:
 
             # recursively visit the next cell
             self._break_walls_r(next_index[0], next_index[1])
+
+    def _reset_cells_visited(self):
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._cells[i][j].visited = False
