402abcbe9c23f218253087defce4a7d41d2cc26e
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-02-20 22:06:36 +0100

    Added the maze class that draws the maze. Updated the Cells and Main to match

--- cell.py
+++ cell.py
@@ -1,40 +1,45 @@
 from graphics import Line, Point
 
 class Cell:
-    def __init__(self, x1, y1, x2, y2, window):
+    def __init__(self, window):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = window
+
+    def draw(self, x1, y1, x2, y2):
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-        self._win = window
 
-    def draw(self):
         # Left Wall
         if self.has_left_wall:
             self._win.draw_line(
-                Line(Point(self._x1, self._y1), Point(self._x1, self._y2)), "black"
+                Line(Point(x1, y1), Point(x1, y2)), "black"
             )
 
         # Top Wall
         if self.has_top_wall:
             self._win.draw_line(
-                Line(Point(self._x1, self._y1), Point(self._x2, self._y1)), "black"
+                Line(Point(x1, y1), Point(x2, y1)), "black"
             )
 
         # Right Wall
         if self.has_right_wall:
             self._win.draw_line(
-                Line(Point(self._x2, self._y1), Point(self._x2, self._y2)), "black"
+                Line(Point(x2, y1), Point(x2, y2)), "black"
             )
 
         # Bottom Wall
         if self.has_bottom_wall:
             self._win.draw_line(
-                Line(Point(self._x1, self._y2), Point(self._x2, self._y2)), "black"
+                Line(Point(x1, y2), Point(x2, y2)), "black"
             )
 
     def draw_move(self, to_cell, undo=False):
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from graphics import Window
-from cell import Cell
+from maze import Maze
 
 def main():
-    win = Window(800, 600)
-    cell1 = Cell(50, 50, 100, 100, win)
-    cell1.has_right_wall = False
-    cell1.draw()
-    cell2 = Cell(100, 50, 150, 100, win)
-    cell2.has_left_wall = False
-    cell2.draw()
-    cell1.draw_move(cell2, True)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+
+    win = Window(screen_x, screen_y)
+    Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
     win.wait_for_close()
 
 main()
