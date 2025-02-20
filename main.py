53cc079ae1de4e21850be2c8a647d6e7615d298f
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-02-20 01:01:25 +0100

    Initialize project with basic window system using Tkinter

---
+++ main.py
@@ -0,0 +1,28 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=True)
+        self.__running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+
+    def close(self):
+        self.__running = False
+
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+main()
